59aafcdb71903574723d8dc2b1a92c3b8e10c602
muhamadfad <98180195+muhamadfad@users.noreply.github.com>
2022-10-16 14:54:00 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,177 +1,172 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
-
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
+#include <math.h>
 #include <GL/glut.h>
-#endif
-
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#define PI 3.14
 
-static int slices = 16;
-static int stacks = 16;
 
-/* GLUT callback Handlers */
 
-static void resize(int width, int height)
-{
-    const float ar = (float) width / (float) height;
+//Deklarasi fungsi Mouse agar gambar 3d dapat diputar putar menggunakan Mouse
+float xrot =0;
+float yrot = 0;
+float xdiff = 0;
+float ydiff = 0;
+bool mouseDown = false;
 
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+//DEKLARASI UNTUK MEMBUAT BENDA BERGERAK
+int gerak = 0;
+bool atas = true;
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
+
+//Deklarasi pengaturan lembaran kerja agar Gambar 3d yang kita buat saat diputar atau di geser tidak kemana mana
+void ukur(int lebar, int tinggi){
+if(tinggi==0) tinggi=1;
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+gluPerspective(45,lebar/tinggi, 5, 450);
+glTranslatef(0,0,-400);// jarak object dari lembaran kerja
+glMatrixMode(GL_MODELVIEW);
+}
+void myinit(void){
+glClearColor (0.0, 0.0, 0.0, 0.0);
+
+glMatrixMode(GL_PROJECTION);
+glEnable(GL_DEPTH_TEST);
+
+glMatrixMode(GL_MODELVIEW);
+glPointSize(10.0);
+glLineWidth(7.0f);
 }
 
-static void display(void)
+//Dan selanjutnya yaitu fungsi mouse
+void idle()
 {
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
-
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
+if (!mouseDown)
+{
+xrot += 0.3;
+yrot += 0.4;
+}
+glutPostRedisplay();
 }
 
-
-static void key(unsigned char key, int x, int y)
+void mouse(int button, int state, int x, int y)
 {
-    switch (key)
-    {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
+if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+{
+mouseDown = true;
+xdiff = x - yrot;
+ydiff = -y + xrot;
+}
+else
+mouseDown = false;
+}
 
-        case '+':
-            slices++;
-            stacks++;
-            break;
+void mouseMotion(int x, int y)
+{
+if(mouseDown)
+{
+yrot = x - xdiff;
+xrot = y + ydiff;
+glutPostRedisplay();
+}
+}
 
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
+//Dibawah ini dimulai koding untuk membuat object
+void tampilan(void){
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+glLoadIdentity();
+gluLookAt(0,0,3,0,0,0,0,1,0);
+glRotatef(xrot, 1, 0,0);
+glRotatef(yrot, 0,1,0);
+glPushMatrix();
+
+glTranslatef( gerak, 0,0);//UNTUK MENGGERAKKAN BENDA
+//KUBUS BAG 1
+glBegin(GL_POLYGON);
+glColor3f(0,0,0);
+glVertex3f(-40,-40,40);
+glVertex3f(40,-40,40);
+glVertex3f(40,-40,-40);
+glVertex3f(-40,-40,-40);
+glEnd();
+//KUBUS BAG 2
+glBegin(GL_POLYGON);
+glColor3f(0,0,0);
+glVertex3f(40,-40,40);
+glVertex3f(40,40,40);
+glVertex3f(40,40,-40);
+glVertex3f(40,-40,-40);
+glEnd();
+//KUBUS BAG 3
+glBegin(GL_POLYGON);
+glColor3f(0,0,0);
+glVertex3f(-40,-40,40);
+glVertex3f(-40,40,40);
+glVertex3f(-40,40,-40);
+glVertex3f(-40,-40,-40);
+glEnd();
+//KUBUS BAG 4
+glBegin(GL_POLYGON);
+glColor3f(0,0,0);
+glVertex3f(-40,40,40);
+glVertex3f(40,40,40);
+glVertex3f(40,40,-40);
+glVertex3f(-40,40,-40);
+glEnd();
+//KUBUS BAG 5
+glBegin(GL_POLYGON);
+glColor3f(0,0,0);
+glVertex3f(-40,40,40);
+glVertex3f(40,40,40);
+glVertex3f(40,-40,40);
+glVertex3f(-40,-40,40);
+glEnd();
+//KUBUS BAG 6
+glBegin(GL_POLYGON);
+glColor3f(0,0,0);
+glVertex3f(-40,40,-40);
+glVertex3f(40,40,-40);
+glVertex3f(40,-40,-40);
+glVertex3f(-40,-40,-40);
+glEnd();
+
+glPushMatrix();
+glPopMatrix();
+glutSwapBuffers();
+}
+void timer(int t)//UNTUK MENGGERAKKAN BENDA
+{
+    if (atas){
+        gerak += 1;
+    } else {
+        gerak -= 1;
     }
-
-    glutPostRedisplay();
+    if (gerak > 20) {
+        atas = false;
+    } else if(gerak < -20){
+        atas = true;
+    }
+ glutPostRedisplay();
+//kecepatan mobil berbanding terbalik
+//semakin besar nilai glutTimerFunc jika ingin mengganti kecepatan
+//silahkan ganti angka 50 di bawah ini, misal 100 mobil akan semakin lambat
+//dibawah 50 mobil akan semakin cepat
+ glutTimerFunc(50,timer,0);
 }
 
-static void idle(void)
-{
-    glutPostRedisplay();
-}
-
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
-
-/* Program entry point */
-
-int main(int argc, char *argv[])
-{
-    glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-
-    glutCreateWindow("GLUT Shapes");
-
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
-
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
+int main(int argc, char **argv){
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowPosition(240, 80);
+    glutInitWindowSize(750, 600);
+    glutCreateWindow("MEMBUAT CPU");
+    myinit();
+    glClearColor(1.0,1.0,1.0,0.0);
+    glutDisplayFunc(tampilan);
+    glutTimerFunc(1,timer,0);//UNTUK MENGGERAKKAN BENDA
+	glutMouseFunc(mouse);
+	glutMotionFunc(mouseMotion);
+    glutReshapeFunc(ukur);
     glutMainLoop();
-
-    return EXIT_SUCCESS;
 }
