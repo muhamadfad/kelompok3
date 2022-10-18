c269ddd6c641287150c3dcfdddd223f279ca4827
Thoga12 <114419883+Thoga12@users.noreply.github.com>
2022-10-18 17:17:02 +0800

    E1E121042_TOGA ABDI HAYAT_BALOK

--- main.cpp
+++ main.cpp
@@ -20,13 +20,45 @@ void lingkarang (int r, int a, int x_tgh, int y_tgh){
     for (i=0;i<=360;i++){
         float sdt=i*(2*PI/a);
         float x=x_tgh+r*cos(sdt);
-        float y=y_tgh+r*sin(sdt)
+        float y=y_tgh+r*sin(sdt);
+        float z=69;
         ;
-    glVertex2f(x,y);
+    glVertex3f(x,y,z);
     }
     glEnd();
 }
+    //atur layar
+    int screen_width=500;//mengatur lebar screen
+    int screen_height=600;//mengatur tinggi screen
+    int button_up=0,button_down=0;
+    int Turn=0;
+ //seting pencahayaan
+    //GLfloat ambient_light[]={0.3,0.3,0.45,1.0};
+    GLfloat ambient_light[]={0.0,0.0,0.45,1.0};//GL_LIGHT0, GL_LIGHT1, GL_LIGHT2, GL_LIGHT3
+    //GLfloat  source_light[]={0.9,0.8,0.8,1.0};
+    GLfloat  source_light[]={0.8,0.8,0.8,1.0};
+    //GLfloat     light_pos[]={7.0,0.0,1.0,1.0};
+    GLfloat     light_pos[]={5.0,0.0,6.0,1.0};
 
+    void init(void)
+    {
+
+    glShadeModel(GL_SMOOTH);
+    glViewport(0,0,screen_width,screen_height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f,(GLfloat)screen_width/(GLfloat)screen_height,1.0f,1000.0f);
+
+    glEnable (GL_DEPTH_TEST);
+    glPolygonMode   (GL_FRONT_AND_BACK,GL_FILL);
+    glEnable (GL_LIGHTING); // pemanggilan parameter lihghting
+    glLightModelfv  (GL_LIGHT_MODEL_AMBIENT,ambient_light);
+    glLightfv (GL_LIGHT0,GL_DIFFUSE,source_light);
+    glLightfv (GL_LIGHT0,GL_POSITION,light_pos);
+    glEnable (GL_LIGHT0);
+    glEnable (GL_COLOR_MATERIAL);
+    glColorMaterial (GL_FRONT,GL_AMBIENT_AND_DIFFUSE);
+    }
 
 //Deklarasi fungsi Mouse agar gambar 3d dapat diputar putar menggunakan Mouse
 float xrot =0;
@@ -113,7 +145,7 @@ glVertex3f(-30,-68,-55); // kiri bawah
 glEnd();
 //KUBUS BAG 2 Hijau
 glBegin(GL_POLYGON);
-glColor3f(0,1,0);
+glColor3f(0.6,0.6,0.6);
 glVertex3f(30,-68,68);
 glVertex3f(30,68,68);
 glVertex3f(30,68,-55);
@@ -121,7 +153,7 @@ glVertex3f(30,-68,-55);
 glEnd();
 //KUBUS BAG 3 Biru
 glBegin(GL_POLYGON);
-glColor3f(0,0,1);
+glColor3f(0.6,0.6,0.6);
 glVertex3f(-30,-68,68);
 glVertex3f(-30,68,68);
 glVertex3f(-30,68,-55);
@@ -129,7 +161,7 @@ glVertex3f(-30,-68,-55);
 glEnd();
 //KUBUS BAG 4 Kuning
 glBegin(GL_POLYGON);
-glColor3f(1,1,0);
+glColor3f(0.7,0.7,0.7);
 glVertex3f(-30,68,68);
 glVertex3f(30,68,68);
 glVertex3f(30,68,-55);
@@ -138,40 +170,84 @@ glEnd();
 
 //KUBUS BAG 5 Biru Muda
 glBegin(GL_POLYGON);
-glColor3f(0,1,1);
-glVertex3f(-30,68,68);
-glVertex3f(30,68,68);
-glVertex3f(30,-68,68);
-glVertex3f(-30,-68,68);
+glColor3f(0.3,0.3,0.3);
+glVertex3f(-26,64,68);
+glVertex3f(26,64,68);
+glVertex3f(26,-64,68);
+glVertex3f(-26,-64,68);
 glEnd();
 //KUBUS BAG 6 Ungu
 glBegin(GL_POLYGON);
-glColor3f(1,0,1);
+glColor3f(0.7,0.7,0.7);
 glVertex3f(-30,68,-55);
 glVertex3f(30,68,-55);
 glVertex3f(30,-68,-55);
 glVertex3f(-30,-68,-55);
 glEnd();
 
-//garis 1
+
+
+//GARIS 1
 glBegin(GL_POLYGON);
-glColor3f(1,0,1);
-glVertex3f(-30,68,68.5);
-glVertex3f(30,68,68.5);
-glVertex3f(30,-6,68.5);
-glVertex3f(-30,-6,68.5);
+glColor3f(0.6,0.6,0.6);
+glVertex3f(-26,50,69);
+glVertex3f(26,50,69);
+glVertex3f(26,47,69);
+glVertex3f(-26,47,69);
 glEnd();
 
-//garis 2
-glLineWidth(10);
-glBegin(GL_LINES);
-glColor3f(0,1,0);
-glVertex3i(-30,40,68);
-glVertex3i(30,40,68);
+//GARIS 2
+glBegin(GL_POLYGON);
+glColor3f(0.6,0.6,0.6);
+glVertex3f(-26,40,69);
+glVertex3f(26,40,69);
+glVertex3f(26,37,69);
+glVertex3f(-26,37,69);
 glEnd();
 
+//GARIS 3
+glBegin(GL_POLYGON);
+glColor3f(0.6,0.6,0.6);
+glVertex3f(-26,30,69);
+glVertex3f(26,30,69);
+glVertex3f(26,27,69);
+glVertex3f(-26,27,69);
+glEnd();
+//buat tombol power linkaran
+glColor3f(0.9,0,0.6);
+lingkarang(10,20,0,-50);
+//garis samping kiri
+glBegin(GL_QUADS);
+glColor3f(0.91,0.91,0.91);
+glVertex3f(-30,68,68);
+glVertex3f(-26,68,68);
+glVertex3f(-26,-68,68);
+glVertex3f(-30,-68,68);
+glEnd();
+//garis samping kanan
+glBegin(GL_QUADS);
+glColor3f(1,1,1);
+glVertex3f(30,68,68);
+glVertex3f(26,68,68);
+glVertex3f(26,-68,68);
+glVertex3f(30,-68,68);
+glEnd();
 
+glBegin(GL_QUADS);
+glColor3f(0.91,0.91,0.91);
+glVertex3f(30,68,68);
+glVertex3f(-26,68,68);
+glVertex3f(-26,64,68);
+glVertex3f(30,64,68);
+glEnd();
 
+glBegin(GL_QUADS);
+glColor3f(0.95,0.95,0.95);
+glVertex3f(30,-68,68);
+glVertex3f(-26,-68,68);
+glVertex3f(-26,-64,68);
+glVertex3f(30,-64,68);
+glEnd();
 glPushMatrix();
 glPopMatrix();
 glutSwapBuffers();
@@ -203,6 +279,7 @@ int main(int argc, char **argv){
     glutInitWindowSize(750, 600);
     glutCreateWindow("MEMBUAT CPU");
     myinit();
+
     glutDisplayFunc(tampilan);
     glutTimerFunc(1,timer,0);//UNTUK MENGGERAKKAN BENDA
 	glutMouseFunc(mouse);
